4310b27c581450248d5dd449054681115102d530
Maxim Britvin <maksbritvin@gmail.com>
2024-07-05 16:05:43 +0300

    add antd

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,12 @@
 
 import "./index.css"
+import { DatePicker } from "antd";
 
 export default function App() {
    return (
-    <p className="text-3xl underline">Hello world! Max</p>
+    <>
+      <p className="text-3xl underline">Hello world! Max</p>
+      <DatePicker />
+    </>
   );
 }

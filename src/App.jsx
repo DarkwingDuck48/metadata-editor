0b6d39c1240d8ad3d2df5ce1fd3288dffd237571
Maxim Britvin <maksbritvin@gmail.com>
2024-07-05 15:46:03 +0300

    test changes

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,6 @@ import "./index.css"
 
 export default function App() {
    return (
-    <p className="text-3xl underline">Hello world! Max</p>
+    <p className="text-3xl underline">Hello world! Max is best</p>
   );
 }
